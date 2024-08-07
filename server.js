fce7262d8e2b9c8096c153ae7e69bcae8db2e397
Divyesh Dudhat <divyeshdudhats@gmail.com>
2024-08-07 09:44:57 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse incoming request bodies as text/html
+app.use(bodyParser.text({ type: 'text/html' }));
+
+app.get('/', (req, res) => {
+    try {
+        // Send the index.html file as a response
+        res.sendFile(path.join(__dirname, '/', 'index.html'));
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/convert', async (req, res) => {
+    const html = req.body;
+
+    if (!html) {
+        return res.status(400).send('No HTML content provided');
+    }
+
+    try {
+        // Launch a new instance of Chromium
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const page = await browser.newPage();
+
+        // Set the content of the page to the provided HTML
+        await page.setContent(html, { waitUntil: 'networkidle0', timeout: 60000 }); // Increased timeout
+
+        // Modify the DOM to replace image alt attributes with src attributes
+        await page.evaluate(() => {
+            const images = document.querySelectorAll('img[alt]');
+            images.forEach(img => {
+                if (img.alt) {
+                    img.src = img.alt;
+                    img.alt = '';
+                }
+            });
+        });
+
+        // Generate PDF from the page content with margins
+        const pdfBuffer = await page.pdf({
+            format: 'A4',
+            timeout: 120000,
+            printBackground: true
+        });
+
+        // Close the browser instance
+        await browser.close();
+
+        // Set the response headers and send the PDF buffer
+        res.set('Content-Type', 'application/pdf');
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error('Error generating PDF:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
